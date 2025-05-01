4e2e6b56657b46bd228d18f2623ac2a396428b0e
Pinhasov_Margarita <45937245+rita10514@users.noreply.github.com>
2025-05-01 22:16:43 +0300

    v1

--- story_content/user.js
+++ story_content/user.js
@@ -2,6 +2,48 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "68q5JPNKw0E":
+        Script1();
+        break;
+      case "5vKWjhasx4n":
+        Script2();
+        break;
+      case "6rATDafnqwo":
+        Script3();
+        break;
   }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+var layerNumber = player.GetVar("layerNumber"); // get the current value
+
+var remainder = layerNumber % 4; // get remainder after division by 4
+
+player.SetVar("remainder", remainder); // save it back to Storyline
+
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+var layerNumber = player.GetVar("layerNumber"); // get the current value
+
+var remainder = layerNumber % 4; // get remainder after division by 4
+
+player.SetVar("remainder", remainder); // save it back to Storyline
+
+}
+
+function Script3()
+{
+  var player = GetPlayer();
+var layerNumber = player.GetVar("layerNumber"); // get the current value
+
+var remainder = layerNumber % 4; // get remainder after division by 4
+
+player.SetVar("remainder", remainder); // save it back to Storyline
+
 }
 
